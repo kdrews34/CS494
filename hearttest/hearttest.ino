6202b99fb7fb5c22ebf69636e58ee6082d7dbdbc
Kevin Andrews <kdrews34@gmail.com>
2019-09-17 16:03:23 -0500

    did stuff

--- hearttest/hearttest.ino
+++ hearttest/hearttest.ino
@@ -1,41 +1,33 @@
-/******************************************************************************
-Heart_Rate_Display.ino
-Demo Program for AD8232 Heart Rate sensor.
-Casey Kuhns @ SparkFun Electronics
-6/27/2014
-https://github.com/sparkfun/AD8232_Heart_Rate_Monitor
-The AD8232 Heart Rate sensor is a low cost EKG/ECG sensor.  This example shows
-how to create an ECG with real time display.  The display is using Processing.
-This sketch is based heavily on the Graphing Tutorial provided in the Arduino
-IDE. http://www.arduino.cc/en/Tutorial/Graph
-Resources:
-This program requires a Processing sketch to view the data in real time.
-Development environment specifics:
-  IDE: Arduino 1.0.5
-  Hardware Platform: Arduino Pro 3.3V/8MHz
-  AD8232 Heart Monitor Version: 1.0
-This code is beerware. If you see me (or any other SparkFun employee) at the
-local pub, and you've found our code helpful, please buy us a round!
-Distributed as-is; no warranty is given.
-******************************************************************************/
+#include "MegunoLink.h"
+#include "FIlter.h"
+
+ExponentialFilter<int> Breath(20,0);
+ExponentialFilter<int> Heart(20,0);
+int bRaw;
+int hRaw;
 
 void setup() {
   // initialize the serial communication:
-  Serial.begin(9600);
   pinMode(10, INPUT); // Setup for leads off detection LO +
   pinMode(11, INPUT); // Setup for leads off detection LO -
-
+  Serial.begin(115200);
 }
 
 void loop() {
-  
-  if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
-    Serial.println('!');
+
+  //print breathing rate
+  bRaw = analogRead(A1);
+  Breath.Filter(bRaw);
+
+  //print heart rate
+  if(!((digitalRead(10) == 1)||(digitalRead(11) == 1))){
+      hRaw = analogRead(A0);
+      Heart.Filter(hRaw);
   }
-  else{
-    // send the value of analog input 0:
-      Serial.println(analogRead(A0));
-  }
+  //print stuff to serial
+  Serial.print(Breath.Current());
+  Serial.print(",");
+  Serial.println(Heart.Current());
   //Wait for a bit to keep serial data from saturating
-  delay(5);
+  delay(20);
 }
